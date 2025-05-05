41d73f9c1d31c8740e796790686b68b4d67f8cba
ObiJuanKenobi2223 <maxvandevisse2@gmail.com>
2025-05-05 16:31:49 +0200

    Quartz sync: May 5, 2025, 4:31 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [Component.Search()],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -41,7 +41,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
