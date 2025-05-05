35e565452bcd6beda3e5eb36c0dd6ef2ca15e016
ObiJuanKenobi2223 <maxvandevisse2@gmail.com>
2025-05-05 18:36:40 +0200

    Quartz sync: May 5, 2025, 6:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-NL",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
